7d3d73bdc946faef5c4abef718688056cd4e29da
hya <hunya406@gmail.com>
2019-05-14 11:29:05 +0800

    delete

--- WEB/web4/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-error_reporting(0);
-$flag="sicnuctf{PHPC0de_Aud1t_FunNy_Byp@s5}";
-?>
--- WEB/web5/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	setcookie("token","",time()-3600);
-	
-	header("Refresh:0;url=index.php");
-
-?>
